c68bccbd0c298b245b9dd0ed5dd1596f1f2d8c33
ArtRakh <art.rakh@gmail.com>
2023-05-23 17:42:50 +0300

    Add predict price method

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import joblib
 import numpy
+import pandas
 
 MODEL_PATH = 'mlmodels/model.pkl'
 SCALER_X_PATH = 'mlmodels/scaler_x.pkl'
@@ -15,11 +16,17 @@ sc_y = joblib.load(SCALER_Y_PATH)
 def predict():
     args = request.args
     open_plan = args.get('open_plan', default=-1, type=int)
+    first_day_exposition = args.get('first_day_exposition', default=-1, type=object)
+    last_day_exposition = args.get('last_day_exposition', default=-1, type=object)
     rooms = args.get('rooms', default=-1, type=int)
     area = args.get('area', default=-1, type=float)
     renovation = args.get('renovation', default=-1, type=int)
 
-    x = numpy.array([open_plan, rooms, area, renovation]).reshape(1,-1)
+    df = pandas.DataFrame([first_day_exposition, last_day_exposition, open_plan, rooms, area, renovation])
+    df['length_exposition'] = (pandas.to_datetime(df.last_day_exposition) - pandas.to_datetime(df.first_day_exposition)).dt.days
+    df = pandas.get_dummies(df, columns=['open_plan', 'rooms', 'renovation'])
+    df.drop(columns=['first_day_exposition', 'last_day_exposition'], inplace=True)
+    x = numpy.array(df).reshape(1,-1)
     x = sc_x.transform(x)
     result = model.predict(x)
     result = sc_y.inverse_transform(result.reshape(1,-1))
